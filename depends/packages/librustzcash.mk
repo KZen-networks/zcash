19897e1887393f472d7ab0bb19587b516fb19249
omershlo <omer.shlomovits@gmail.com>
2019-06-01 19:36:54 +0300

    debug

--- depends/packages/librustzcash.mk
+++ depends/packages/librustzcash.mk
@@ -3,8 +3,8 @@ $(package)_version=0.1
 $(package)_download_path=https://github.com/omershlo/$(package)/archive/
 $(package)_file_name=$(package)-$($(package)_git_commit).tar.gz
 $(package)_download_file=$($(package)_git_commit).tar.gz
-$(package)_sha256_hash=a2f13992789717e5c3f1cc23ac874b9a26a703c473553eaccbc2db09ce01038f
-$(package)_git_commit=139f1ae815c80153a68f77648ed8cb3e9dad4d6c
+$(package)_sha256_hash=497d140d60549d7cb27e7cd38998808c414f6965c9b20865ebcd32364f5dbca9
+$(package)_git_commit=ac38c4fdc2b689fd9d8a4b0f9791eabb47cfa026
 $(package)_dependencies=rust $(rust_crates)
 $(package)_patches=cargo.config
 
